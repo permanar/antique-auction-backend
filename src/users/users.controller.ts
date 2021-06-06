d7cf94b2d5f0f86911841e1adac7f531d2a5da77
Richie Permana <richie.permana@gmail.com>
2021-06-07 04:05:06 +0800

    feat: Adding Business Logic for the Users Endpoint
    - Added auto-bid bot support logic inside users service.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+export type DataAutoBid = {
+  _id: string;
+  id_bid: string;
+  bot: boolean;
+  maxAmount: number;
+  alertAmount: number;
+};
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(id, updateUserDto);
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @Patch('/auto-bid')
+  autoBid(@Body() data: DataAutoBid) {
+    return this.usersService.autoBidding(data);
+  }
+}
